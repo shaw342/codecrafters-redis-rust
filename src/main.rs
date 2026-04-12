6b182188543ebe6cdabba6301f08d86c69a8bfc2
shawan.barua <shawan.barua@ecole2600.com>
2026-04-12 12:31:39 +0200

    first test for implement PING

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -7,12 +8,23 @@ fn main() {
 
     // Uncomment the code below to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6380").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                let mut buffer = [0; 512];
+
+                match _stream.read(&mut buffer) {
+                    Ok(n) => {
+                        if &buffer[0..n] == b"PING" {
+                            let _ = _stream.write_all(b"+PONG\r\n");
+                        }
+                    }
+                    Err(e) => {
+                        println!("error: {}", e)
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);

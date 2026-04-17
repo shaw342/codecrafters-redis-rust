f74ee1aba4c8cf59da211a1baf75aef52d977388
shawan.barua <shawan.barua@ecole2600.com>
2026-04-17 17:57:49 +0200

    asynchrone thread with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
-#![allow(unused_imports)]
-use std::io::{Read, Write};
-use std::net::TcpListener;
-use tokio::spawn;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener; // Import nécessaire pour .read() et .write_all()
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    // Uncomment the code below to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    loop {
+        // 1. On accepte la connexion de manière asynchrone
+        let (mut socket, _) = listener.accept().await?;
 
-    for stream in listener.incoming() {
-        spawn(async move {
-            match stream {
-                Ok(mut _stream) => loop {
-                    let mut buffer = [0; 512];
+        // 2. On spawn une tâche pour ce client
+        tokio::spawn(async move {
+            let mut buffer = [0; 512];
 
-                    match _stream.read(&mut buffer) {
-                        Ok(_n) => {
-                            let _ = _stream.write_all(b"+PONG\r\n");
-                        }
-                        Err(e) => {
-                            println!("error: {}", e)
+            loop {
+                // 3. On lit de manière asynchrone
+                match socket.read(&mut buffer).await {
+                    Ok(0) => return, // Connexion fermée par le client
+                    Ok(_) => {
+                        // 4. On écrit de manière asynchrone
+                        if let Err(e) = socket.write_all(b"+PONG\r\n").await {
+                            eprintln!("Erreur lors de l'écriture : {}", e);
+                            return;
                         }
                     }
-                },
-
-                Err(e) => {
-                    println!("error: {}", e);
+                    Err(e) => {
+                        eprintln!("Erreur lors de la lecture : {}", e);
+                        return;
+                    }
                 }
             }
         });

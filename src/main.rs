d1d7900f18a4e1fa6afe16242a7c598793d5950b
shawan.barua <shawanbarua70@gmail.com>
2026-04-17 23:40:30 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener; // Import nécessaire pour .read() et .write_all()
+use tokio::net::TcpListener;
+
+fn parse_command(buf: &[u8]) -> String {
+    let data = String::from_utf8_lossy(buf);
+    return data
+        .split("\r\n")
+        .filter(|s| !s.starts_with("*") && !s.starts_with("$") && !s.is_empty())
+        .collect();
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     loop {
-        // 1. On accepte la connexion de manière asynchrone
         let (mut socket, _) = listener.accept().await?;
 
-        // 2. On spawn une tâche pour ce client
         tokio::spawn(async move {
             let mut buffer = [0; 512];
 
             loop {
-                // 3. On lit de manière asynchrone
                 match socket.read(&mut buffer).await {
-                    Ok(0) => return, // Connexion fermée par le client
-                    Ok(_) => {
-                        // 4. On écrit de manière asynchrone
+                    Ok(0) => return,
+                    Ok(_n) => {
+                        let cmd = parse_command(&mut buffer);
+                        match cmd {
+                            cmd => {
+                                if cmd == "ECHO" {
+                                    eprintln!("PONG");
+                                }
+                            }
+                        }
+
                         if let Err(e) = socket.write_all(b"+PONG\r\n").await {
                             eprintln!("Erreur lors de l'écriture : {}", e);
                             return;
@@ -31,5 +44,21 @@ async fn main() -> anyhow::Result<()> {
                 }
             }
         });
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn feature_parse_command() {
+        let str = parse_command(b"*1\r\n$4\r\nPING\r\n");
+
+        assert_eq!(
+            "PING", str,
+            "we are testing addition with {} and {}",
+            "PING", str
+        );
     }
 }

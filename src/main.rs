bcc6a73f670f1a85587496cffe9eb0a337575296
shawan.barua <shawan.barua@ecole2600.com>
2026-04-17 17:00:41 +0200

    asynchrone thread with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use tokio::spawn;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,24 +12,25 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => loop {
-                let mut buffer = [0; 512];
+        spawn(async move {
+            match stream {
+                Ok(mut _stream) => loop {
+                    let mut buffer = [0; 512];
 
-                match _stream.read(&mut buffer) {
-                    Ok(_n) => {
-                        let _ = _stream.write_all(b"+PONG\r\n");
+                    match _stream.read(&mut buffer) {
+                        Ok(_n) => {
+                            let _ = _stream.write_all(b"+PONG\r\n");
+                        }
+                        Err(e) => {
+                            println!("error: {}", e)
+                        }
                     }
+                },
 
-                    Err(e) => {
-                        println!("error: {}", e)
-                    }
+                Err(e) => {
+                    println!("error: {}", e);
                 }
-            },
-
-            Err(e) => {
-                println!("error: {}", e);
             }
-        }
+        });
     }
 }
